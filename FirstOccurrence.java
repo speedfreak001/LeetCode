ff41b1255647049830150a9bb3b7b9699feeca84
Ravi kumar <42580970+speedfreak001@users.noreply.github.com>
2023-06-28 11:13:45 +0530

    Create FirstOccurrence.java

---
+++ FirstOccurrence.java
@@ -0,0 +1,20 @@
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+        if(haystack.length() == 1) {
+            if(haystack.charAt(0)==needle.charAt(0)) {
+                return 0;
+            }
+        }
+        for (int i = 0; i< haystack.length(); i++) {
+            if (haystack.startsWith(needle, i)) {
+                 return i;
+            }
+        }
+        return -1;
+    }
+}
